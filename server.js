1c6c9c0c37fa77ecbfcbaa973a7c31dbf73998cd
Marko <129887087+Marko010101@users.noreply.github.com>
2024-11-06 18:54:07 +0400

    FEAT!: Add CRUD operations for tours with Mongoose, including schema, routing, and database setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app.js");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("DB connection successful!"))
+  .catch((err) => console.error("DB connection error:", err));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
