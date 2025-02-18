a22b9d12e4c35c95473ab4087ff640c9a3a54307
Marko <129887087+Marko010101@users.noreply.github.com>
2025-02-18 19:13:57 +0400

    Added heroku config

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const reviewRouter = require("./routes/reviewRoutes.js");
 
 const app = express();
 
+app.enable("trust proxy");
+
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
